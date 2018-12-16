2cbfcf72408dfddfc1d6b6721d4bfbd6b2ec1bd8
liuxiaoyu-git <xiaoyliu@redhat.com>
2018-12-16 14:39:52 +0800

    Update load-all.sh

--- example1/load-all.sh
+++ example1/load-all.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 oc login 10.2.2.2:8443 -u example -p pass
+git clone https://github.com/liuxiaoyu-git/ose-app-promotion-configmap.git
+cd ose-app-promotion-configmap/example1
 oc new-project configmap-example
 oc create -f configmap-example.json
 oc create -f node-app-deployment.json
